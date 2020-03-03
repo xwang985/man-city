02834d5f2d058a9c1ed70f8480b82a9fb9eee635
xwang985 <39933823+xwang985@users.noreply.github.com>
2020-03-03 18:39:53 -0500

    removed redundant component

--- src/index.js
+++ src/index.js
@@ -18,6 +18,3 @@ firebase.auth().onAuthStateChanged(user => {
   ReactDOM.render(<App user={user} />, document.getElementById("root"));
 });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
